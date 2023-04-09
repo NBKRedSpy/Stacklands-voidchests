81464c84c4bbadf2e6b17605579de8defcffa3e2
bo0tzz <git@bo0tzz.me>
2023-04-09 18:09:38 +0200

    Set up project structure

---
+++ VoidChests.cs
@@ -0,0 +1,15 @@
+using BepInEx;
+using BerryLoaderNS;
+
+namespace VoidChests
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("BerryLoader")]
+    class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
